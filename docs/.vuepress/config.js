590df363dcc32417e43b66422e800bd66c9127d9
jack9603301 <jack9603301@163.com>
2022-05-09 15:05:21 +0800

    init vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  lang: 'zh-CN',
+  title: '你好， VuePress ！',
+  description: '这是我的第一个 VuePress 站点',
+  theme: 'vuepress-theme-maker',
+  themeConfig: {
+    // Please head documentation to see the available options.
+  }
+}
